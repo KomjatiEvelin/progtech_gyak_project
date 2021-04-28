7243ff4fc0d5661b751ddf994d808843b614b6c9
KomjatiEvelin <komjatievelin@gmail.com>
2021-04-28 17:35:47 +0200

    add, getOne és getList metódusok hozzáadása az interface-hez, minden kezelendő objektumhtípushoz

--- src/main/java/hu/komjati/Warehouse.java
+++ src/main/java/hu/komjati/Warehouse.java
@@ -1,4 +1,23 @@
 package hu.komjati;
 
+
+import java.util.List;
+
 public interface Warehouse {
+
+   boolean addProduct(Product p);
+   List<Product> getProductsList();
+    Product getProductByID(int ID);
+
+   boolean recordNewOrder(Order o);
+    List<Order> getAllOrder();
+    Order getOrderByID(int ID);
+
+    boolean addSupplier(Supplier s);
+    List<Supplier> getAllSuppliers();
+    Supplier getSupplierByID(int ID);
+
+    boolean addCustomer(Customer c);
+    List<Customer> getAllCustomers();
+    Customer getCustomerByID(int ID);
 }
