eb2ffc08e5426afcd7baedbae10bc6d876cd705e
KomjatiEvelin <komjatievelin@gmail.com>
2021-05-08 12:41:01 +0200

    GroceriesWarehouse hozzáadáas Groceries-eket fog kezelni

---
+++ src/main/java/hu/komjati/GrocerieWarehouse.java
@@ -0,0 +1,73 @@
+package hu.komjati;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrocerieWarehouse implements Warehouse {
+
+    private List<Groceries> products;
+
+    public GrocerieWarehouse() {
+        this.products = new ArrayList<>();
+    }
+
+    @Override
+    public boolean addProduct(Product p) {
+        return false;
+    }
+
+    @Override
+    public List<Product> getProductsList() {
+        return null;
+    }
+
+    @Override
+    public Product getProductByID(int ID) {
+        return null;
+    }
+
+    @Override
+    public boolean recordNewOrder(Order o) {
+        return false;
+    }
+
+    @Override
+    public List<Order> getAllOrder() {
+        return null;
+    }
+
+    @Override
+    public Order getOrderByID(int ID) {
+        return null;
+    }
+
+    @Override
+    public boolean addSupplier(Supplier s) {
+        return false;
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliers() {
+        return null;
+    }
+
+    @Override
+    public Supplier getSupplierByID(int ID) {
+        return null;
+    }
+
+    @Override
+    public boolean addCustomer(Customer c) {
+        return false;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public Customer getCustomerByID(int ID) {
+        return null;
+    }
+}
